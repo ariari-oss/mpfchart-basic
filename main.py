f3675f9c631abd0c86fad85d85fedb7b64125cc3
川﨑有紗 <kawasakiarisa@kawasakiarisanoMacBook.local>
2023-09-19 12:01:48 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import mplfinance as mpf
+import yfinance as yf
+
+
+
+st.title("basic chart")
+st.text("銘柄コードを入力してください。")
+aa = st.text_input("","^N225")
+ds = yf.download(aa).tail(100)
+
+
+fig, ax = mpf.plot(ds,
+                   title=aa,
+                   type="candle",
+                   volume=True,
+                   mav=[5,10,60],
+                   figratio=[2,1],
+                   style="yahoo",
+
+
+                   returnfig=True
+                   )
+
+st.pyplot(fig)
+#st.dataframe(ds)
+
