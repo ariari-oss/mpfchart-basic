ab262bafde73c6d844570a3805e438673de1ca4f
川﨑有紗 <chuanqiyousha2@gmail.com>
2023-10-09 14:13:58 +0900

    commit3

--- main.py
+++ main.py
@@ -5,17 +5,28 @@ import yfinance as yf
 
 
 st.title("basic chart")
-st.text("銘柄コードを入力してください。")
-aa = st.text_input("","^N225")
 
-st.markdown('---')
+st.text("ティッカー、証券コードを入力してください\n" +
+        "例）AAPL,7203.T,^N225,JPY=X,BTC-USD")
+#ticker = st.text_input("ティッカーコード:",'^N225')
+if st.checkbox("自分で銘柄を指定する", value=False):
+    ticker = st.text_input("ティッカーコード:")
+else:
+    ticker = st.selectbox("ティッカーコード:",options=['^N225','AAPL','JPY=X','BTC-USD'], index=0)
 
 
-if aa:
-    ds = yf.download(aa).tail(100)
+st.write('&nbsp;')
+show_dataset = st.checkbox('データ表示', False)
+    
+dsp_days = st.slider("表示日数（日）:",min_value=30,max_value=1000,value=100)   
+st.markdown("---")
+
+
+if ticker:
+    ds = yf.download(ticker).tail(dsp_days)
 
     fig, ax = mpf.plot(ds,
-                    title=aa,
+                    title=ticker,
                     type="candle",
                     volume=True,
                     mav=[5,10,60],
@@ -23,8 +34,10 @@ if aa:
                     style="yahoo",
                     returnfig=True
                     )
+    
 
     st.pyplot(fig)
     st.markdown('---')
-    st.dataframe(ds)
+    if show_dataset:
+        st.dataframe(ds)
 
